5a2ac9eafc3cb684e282de4e6a04f838703f7eb5
Anderson Lima <anderson.lima@VILA-SUL.com>
2023-12-20 10:18:27 -0300

    comentarios finais

--- cliente.py
+++ cliente.py
@@ -1,51 +1,63 @@
 import threading
 import socket
 
-
 def main():
-
+    # Criação do socket do cliente
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
+        # Conexão do cliente ao servidor na porta 7777
         client.connect(('localhost', 7777))
     except:
-        return print('\nNão foi possívvel se conectar ao servidor!\n')
+        # Trata a exceção se a conexão não for bem-sucedida
+        return print('\nNão foi possível se conectar ao servidor!\n')
 
+    # Solicitação do nome de usuário ao cliente
     username = input('Digite o Nome Do Usuário> ')
     print('\nConectado')
     print('\nDigite /sair para desconectar')
-    
+    print('Digite /listar_usuarios para obter a lista de usuários online')
 
+    # Inicia duas threads: uma para receber mensagens e outra para enviar mensagens
     thread1 = threading.Thread(target=receiveMessages, args=[client])
     thread2 = threading.Thread(target=sendMessages, args=[client, username])
 
     thread1.start()
     thread2.start()
 
-
 def receiveMessages(client):
+    # Função para receber mensagens do servidor
     while True:
         try:
+            # Recebe e decodifica a mensagem do servidor
             msg = client.recv(2048).decode('utf-8')
             print(msg+'\n')
         except:
+            # Trata a exceção se não for possível permanecer conectado
             print('\nNão foi possível permanecer conectado no servidor!\n')
             print('Pressione <Enter> Para continuar...')
             client.close()
             break
 
-
 def sendMessages(client, username):
+    # Função para enviar mensagens para o servidor
     while True:
         try:
+            # Solicitação da mensagem do usuário
             msg = input('\n')
-            client.send(f'<{username}> {msg}'.encode('utf-8'))
-            if msg == "/sair":    # requisição para sair do chat , cliente manda "/sair"
-                client.send(f'<{username}> {msg}'.encode('utf-8')) 
+            if msg == "/sair":
+                # Envia mensagem de desconexão ao servidor
+                client.send(f'<{username}> {msg}'.encode('utf-8'))
                 client.close()
                 return print("Você saiu do chat")
+            elif msg == "/listar_usuarios":
+                # Envia solicitação de listar usuários online ao servidor
+                client.send(msg.encode('utf-8'))
+            else:
+                # Envia a mensagem normalmente ao servidor
+                client.send(f'<{username}> {msg}'.encode('utf-8'))
         except:
+            # Trata a exceção se ocorrer um erro durante o envio da mensagem
             return
-
 
 main()
--- servidor.py
+++ servidor.py
@@ -1,51 +1,112 @@
 import threading
 import socket
 
+# Lista para armazenar os clientes conectados
 clients = []
 
 def main():
-
+    # Criação do socket do servidor
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
+        # Associa o servidor à porta 7777 e inicia a escuta
         server.bind(('localhost', 7777))
         server.listen()
         print("Servidor iniciado")
+        print("\n-------------COMANDOS--------")
+        print("\n/remove_all : Remover todos os usuários conectados")
+        print("/users : Retorna os usuários e a quantidade de clientes conectados")
     except:
+        # Trata a exceção se não for possível iniciar o servidor
         return print('\nNão foi possível iniciar o servidor!\n')
 
+    # Adicionando uma thread para leitura de mensagens do servidor
+    thread_server_input = threading.Thread(target=serverInput)
+    thread_server_input.start()
+
     while True:
+        # Aceita a conexão do cliente
         client, addr = server.accept()
         clients.append(client)
 
+        # Inicia uma thread para tratar as mensagens do cliente
         thread = threading.Thread(target=messagesTreatment, args=[client])
         thread.start()
 
 def messagesTreatment(client):
-    end = "/sair"
+    # Função para tratar as mensagens recebidas do cliente
     while True:
         try:
-            msg = client.recv(2048)
-            accept_msg = msg.decode('utf-8')
-            if end in accept_msg:             # servidor faz a Confirmação de desconexão do cliente
+            # Recebe e decodifica a mensagem do cliente
+            msg = client.recv(2048).decode('utf-8')
+
+            if "/sair" in msg:
+                # Se a mensagem for "/sair", desconecta o cliente
+                client.close()
+                deleteClient(client)
                 return print("Cliente foi desconectado")
-            broadcast(msg, client)
-            print(accept_msg)
+
+            elif "/listar_usuarios" in msg:
+                # Envia a lista de usuários online ao cliente que solicitou
+                online_users = ", ".join([f"{c.getpeername()[0]}" for c in clients])
+                response = f"Usuários Online: {online_users}"
+                client.send(response.encode('utf-8'))
+
+            else:
+                # Transmite a mensagem para todos os clientes conectados
+                broadcast(msg, client)
+                print(msg)
+
         except:
+            # Se ocorrer um erro, remove o cliente da lista e encerra o loop
             deleteClient(client)
             break
 
-
 def broadcast(msg, client):
+    # Função para enviar mensagens para todos os clientes, exceto o remetente
     for clientItem in clients:
         if clientItem != client:
             try:
-                clientItem.send(msg)
+                # Envia a mensagem codificada em UTF-8
+                clientItem.send(msg.encode('utf-8'))
             except:
+                # Se ocorrer um erro ao enviar, remove o cliente da lista
                 deleteClient(clientItem)
 
-
 def deleteClient(client):
+    # Função para remover um cliente da lista
     clients.remove(client)
 
+# Função para leitura de mensagens do servidor
+def serverInput():
+    while True:
+        try:
+            # Solicitação de comando do servidor a partir da entrada do console
+            msg = input()
+            if msg == "/remove_all":
+                # Remove todos os clientes conectados
+                remove_all_clients()
+                print("Todos os usuários foram removidos.")
+                
+            elif msg == "/users":
+                # Retorna a quantidade e a lista de usuários conectados
+                count_users = len(clients)
+                online_users = ", ".join([f"{c.getpeername()[0]}" for c in clients])
+                response = f"Total de Usuários (Servidor): {count_users}\nUsuários Online: {online_users}"
+                print(response)
+                
+            else:
+                # Transmite a mensagem para todos os clientes conectados
+                broadcast(f"Servidor: {msg}", None)
+        except KeyboardInterrupt:
+            # Trata a interrupção do teclado (Ctrl+C)
+            break
+
+def remove_all_clients():
+    # Função para desconectar e remover todos os clientes da lista
+    for client in clients:
+        client.close()
+    clients.clear()
+
+# Inicia a execução do servidor
 main()
